c413e69151f7739bb3eeee1239024fa6a244fed3
Alec <215294797+justalec27@users.noreply.github.com>
2025-08-29 15:05:20 +0200

    create the right url call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./styles.css"
+import { createUrl,fetchData } from "./logic";
+/*
+Info needed from the api:
+address
+currentConditions .conditions , .sunrise. , .sunset, .feelslike, .humidity
 
-console.log("hello there")
+
+*/
+
+const city = "london"
+
+const location = createUrl(city).url
+
+console.log(location)
+
+const random = fetchData(location).address
+
+console.log(random)
+
+// async function main() {
+//   const data = await fetchData(location); // wait until fetchData finishes
+//     console.log(data.location); // now works!
+// }
+
+
+// main()
+// const lala = main();
+
+// const location = document.querySelector(".location")
+// location.textContent = lala.data.location
---
+++ src/logic.js
@@ -0,0 +1,47 @@
+// Create URL
+export function createUrl (city) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=us&key=SRJBDRP83HWD6GER6HMCFK5KG&contentType=json` 
+
+    console.log(url)
+    return {
+        url,
+    }
+}
+
+
+//Fetch data from API
+export async function fetchData (url){
+    const response = await fetch(
+         url, 
+        {mode: 'cors'});
+
+    const data = await response.json();
+
+      console.log(data);
+      console.log(data.address);
+      console.log(data.currentConditions.datetime)
+      console.log(data.currentConditions.feelslike);
+      console.log(data.currentConditions.humidity);
+      console.log(data.currentConditions.conditions);
+      console.log(data.currentConditions.sunrise);
+      console.log(data.currentConditions.sunset);
+    
+    const location = data.address;
+    const time = data.currentConditions.datetime;
+    const feelsLike = data.currentConditions.feelslike;
+    const humidity = data.currentConditions.humidity;
+    const conditions = data.currentConditions.conditions;
+    const sunrise = data.currentConditions.sunrise;
+    const sunset = data.currentConditions.sunset;
+
+
+     return {
+        location,
+        time, 
+        feelsLike,
+        humidity,
+        conditions,
+        sunrise,
+        sunset,
+      }
+}
